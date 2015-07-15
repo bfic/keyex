38fe1dc72e9b0f8948780f969486c0dfda7ddfcd
bfic <fic.bartosz@gmail.com>
2015-07-15 19:08:14 +0200

    first true commit

--- keyex.sh
+++ keyex.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#### dla nova usera
 
+# for nova user
 usermod -s /bin/bash nova
 su nova
-ssh-keygen
+ssh-keygen -t rsa
 
 cat /var/lib/nova/.ssh/id_rsa.pub  >> /var/lib/nova/.ssh/authorized_keys
 echo 'StrictHostKeyChecking no' >> /var/lib/nova/.ssh/config
-echo "Copying /var/lib/nova/.ssh/id_rsa.pub  >> /var/lib/nova/.ssh/authorized_keys ..."
+
+local_hostname=`hostname`
 
 for x in `seq 1 12`
 do
-  hostname="cisco-cn-$x"
-  scp /var/lib/nova/.ssh/id_rsa.pub root@$hostname:/var/lib/nova/.ssh/authorized_keys
-  ssh root@$hostname | chown nova:nova /var/lib/nova/.ssh/*
+  remote_hostname="hostname-$x"
+  if [ $local_hostname != $remote_hostname ]
+  then
+    echo "[$remote_hostname] Copying /var/lib/nova/.ssh/id_rsa.pub  >> /var/lib/nova/.ssh/authorized_keys ..."
+    cat /var/lib/nova/.ssh/id_rsa.pub | ssh root@$remote_hostname "cat >>  /var/lib/nova/.ssh/authorized_keys && chown nova:nova /var/lib/nova/.ssh/*"
+  fi
 done   
                                                                                        
